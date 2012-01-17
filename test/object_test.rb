de1c3e636882b262640821d98b935be6c4be08eb
macournoyer <macournoyer@gmail.com>
2012-01-17 13:43:38 -0500

    Fix test

--- test/object_test.rb
+++ test/object_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/test_helper'
 class ObjectTest < Test::Unit::TestCase
   def test_lookup
     proto = Mio::Object.new
-    object = proto.clone
+    object = Mio::Object.new(proto)
     
     proto["x"] = 1
     assert_equal 1, object["x"]
